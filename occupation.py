4ad778cea0f479d2460df18026467b4841117f87
bleung329 <bleung@stuy.edu>
2017-09-15 08:32:28 -0400

    Cleaned up

--- occupation.py
+++ occupation.py
@@ -7,14 +7,20 @@ import csv, random
 
 #def random_job():
 
+
 def make_dict(csv_in):
-	csv_dict = {}	
+	csv_dict = {}
 	with open(csv_in, 'rb') as csvfile:
 		occreader = csv.reader(csvfile)
+		#This part eliminates the "Job Class, Percentage"
+		#It also makes the dictionary
 		for row in occreader:
-			if (row[0] == 'Job Class') or (row[0] == 'Total'):
-				continue
-			csv_dict[row[0]] = float(row[1])
+			try:	
+				csv_dict[row[0]] = float(row[1])
+			except:
+				pass
+	#This removes 'Total'
+	del csv_dict['Total']
 	return csv_dict
 
 job_dict = make_dict('occupations.csv')

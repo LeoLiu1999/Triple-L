978bc7075bf5e08beb3f830e37c9445df5648ce9
bleung329 <bleung@stuy.edu>
2017-09-14 22:04:09 -0400

    finished make_dict

--- occupation.py
+++ occupation.py
@@ -1,6 +1,21 @@
-import csv
+'''
+Brian Leung
++
+Leo Liu
+'''
+import csv, random
 
-with open('occupations.csv', 'rb') as csvfile:
-	occreader = csv.reader(csvfile)
-	for row in occreader:
-		print row
+def random_job():
+
+def make_dict(csv_in):
+	csv_dict = {}	
+	with open(csv_in, 'rb') as csvfile:
+		occreader = csv.reader(csvfile)
+		for row in occreader:
+			if (row[0] == 'Job Class') or (row[0] == 'Total'):
+				continue
+			csv_dict[row[0]] = float(row[1])
+	return csv_dict
+
+job_dict = make_dict('occupations.csv')
+print(job_dict)
